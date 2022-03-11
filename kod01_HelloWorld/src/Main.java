98853252d1a7230287626f3656e37b6b8c4e8556
Ahmet Kılınç <95951871+ahmetpoyrazklnc@users.noreply.github.com>
2022-03-12 00:21:17 +0300

    Add files via upload

---
+++ kod01_HelloWorld/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // burada ise temelinde bir hello world yazısını yazdırdık
+        System.
+                out.
+                println
+                        ("Hello World 2");  // noktalı virgül burada satır bitimini ifade etmektedir.
+        // yani pyhton gibi tek tek olarak algılamayıp noktalı virgülü koyduğunuz zaman
+        // satırın bittiğini ifade etmektedir.
+
+        // Javada yorum satırları yan yana iki çizgilerle oluşturulur;
+
+    }
+}
